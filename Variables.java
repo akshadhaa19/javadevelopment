02f295de460135158095789a649883bfd979fdea
Akshadha R <84324430+akshadhaa19@users.noreply.github.com>
2025-01-10 13:57:15 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables{
+public static void main(String[] args) {
+    int passengers= 5;
+    System.out.println(passengers);
+    int BusTickets=passengers;
+    System.out.println(BusTickets);
+
+}
+}
