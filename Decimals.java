c0d3b51e6f78dd47eaf74931a0661936b9841ea3
Akshadha R <84324430+akshadhaa19@users.noreply.github.com>
2025-01-10 16:19:47 +0530

    Add files via upload

---
+++ Decimals.java
@@ -0,0 +1,9 @@
+public class Decimals {
+    public static void main(String[] args) {
+        double percent=34.7;
+        System.out.println("The percentage is " +percent );
+        double wallet=20;
+        int people=3;
+        System.out.println(wallet/people);
+    }
+}
