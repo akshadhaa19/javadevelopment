65c6c9bdf78d0d150055a6cf3ba88a9dad701b2d
Akshadha R <84324430+akshadhaa19@users.noreply.github.com>
2025-01-10 16:22:08 +0530

    Add files via upload

---
+++ WholeNumbers.java
@@ -0,0 +1,8 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long globalPopulation = 80000000L;
+        System.out.println("The global Population is " + globalPopulation);
+        long dailyGoogleSearches = 8500000000L;
+        System.out.println("Daily there are "+dailyGoogleSearches + " google searches");
+    }
+}
